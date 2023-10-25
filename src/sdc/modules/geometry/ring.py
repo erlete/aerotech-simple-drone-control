476b5d5515b0b9914b64c32cfa143c036430cac7
Paulo Sánchez <dev.szblzpaulo@gmail.com>
2023-10-26 00:20:30 +0200

    Update `Ring` class
    Added ring scale, remove center as it was the same as position and also fixed ring rotation issues.

--- src/sdc/modules/geometry/ring.py
+++ src/sdc/modules/geometry/ring.py
@@ -11,12 +11,14 @@ class Ring:
         self,
         position: Vector3D = Vector3D(0, 0, 0),
         rotation: Rotator3D = Rotator3D(0, 0, 0),
+        scale: Vector3D = Vector3D(1, 1, 1),
         height: Union[int, float] = 1,
         hole_radius: Union[int, float] = 5,
         complexity: int = 50
     ) -> None:
         self.position = position
         self.rotation = rotation
+        self.scale = scale
         self.height = height
         self.hole_radius = hole_radius
         self.complexity = complexity
@@ -72,6 +74,31 @@ class Ring:
             )
 
         self._rotation = value
+
+    @property
+    def scale(self) -> Vector3D:
+        """Get ring scale.
+
+        Returns:
+            Vector3D: ring scale.
+        """
+        return self._scale
+
+    @scale.setter
+    def scale(self, value: Vector3D) -> None:
+        """Set ring scale.
+
+        Args:
+            value (Vector3D): ring scale.
+        """
+        if not isinstance(value, Vector3D):
+            raise TypeError(
+                "expected type Vector3D for"
+                + f" {self.__class__.__name__}.scale but got"
+                + f" {type(value).__name__} instead"
+            )
+
+        self._scale = value
 
     @property
     def height(self) -> float:
@@ -149,15 +176,6 @@ class Ring:
         self._complexity = int(value)
 
     @property
-    def center(self) -> np.ndarray:
-        """Get ring geometry center.
-
-        Returns:
-            np.ndarray: ring geometry center.
-        """
-        return self._center
-
-    @property
     def surface(self) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
         """Get ring geometry surface.
 
@@ -176,29 +194,28 @@ class Ring:
         y = (self._hole_radius + self._height * np.cos(theta)) * np.sin(phi)
         z = self._height * np.sin(theta)
 
+        azimuth, elevation, yaw = self._rotation
+
+        # FIXME: turn this into a numpy matrix.
+        x, y, z = np.cos(azimuth) * np.cos(elevation) * x + \
+            (np.cos(azimuth) * np.sin(elevation) * np.sin(yaw) - np.sin(azimuth) * np.cos(yaw)) * y + \
+            (np.cos(azimuth) * np.sin(elevation) * np.cos(yaw) + np.sin(azimuth) * np.sin(yaw)) * z, \
+            np.sin(azimuth) * np.cos(elevation) * x + \
+            (np.sin(azimuth) * np.sin(elevation) * np.sin(yaw) + np.cos(azimuth) * np.cos(yaw)) * y + \
+            (np.sin(azimuth) * np.sin(elevation) * np.cos(yaw) - np.cos(azimuth) * np.sin(yaw)) * z, \
+            -np.sin(elevation) * x + np.cos(elevation) * np.sin(yaw) * y + np.cos(elevation) * np.cos(yaw) * z
+
+        x *= self._scale.x
+        y *= self._scale.y
+        z *= self._scale.z
+
         x += self._position.x
         y += self._position.y
         z += self._position.z
 
-        # azimuth, elevation, yaw = self._rotation
-
-        # # FIXME: turn this into a numpy matrix.
-        # x, y, z = np.cos(azimuth) * np.cos(elevation) * x + \
-        #     (np.cos(azimuth) * np.sin(elevation) * np.sin(yaw) - np.sin(azimuth) * np.cos(yaw)) * y + \
-        #     (np.cos(azimuth) * np.sin(elevation) * np.cos(yaw) + np.sin(azimuth) * np.sin(yaw)) * z, \
-        #     np.sin(azimuth) * np.cos(elevation) * x + \
-        #     (np.sin(azimuth) * np.sin(elevation) * np.sin(yaw) + np.cos(azimuth) * np.cos(yaw)) * y + \
-        #     (np.sin(azimuth) * np.sin(elevation) * np.cos(yaw) - np.cos(azimuth) * np.sin(yaw)) * z, \
-        #     -np.sin(elevation) * x + np.cos(elevation) * np.sin(yaw) * y + np.cos(elevation) * np.cos(yaw) * z
-
-        self._center = Vector3D(np.mean(x), np.mean(y), np.mean(z))
         self._surface = (x, y, z)
 
     def plot(self, ax, **kwargs) -> None:
-        # ax2.set_zlim(-3,3)
-        ax.axis('equal')
-        # ax2.axis('off')
-
         kwargs_ = {
             "rstride": 5,
             "cstride": 5,
@@ -213,54 +230,3 @@ class Ring:
         )
 
         self._position.plot(ax, ".", color=kwargs_["color"], ms=10)
-
-        def Rx(theta):
-            return np.matrix([
-                [ 1, 0, 0],
-                [0, np.cos(theta),-np.sin(theta)],
-                [0, np.sin(theta), np.cos(theta)]
-            ])
-
-        def Ry(theta):
-            return np.matrix([
-                [ np.cos(theta), 0, np.sin(theta)],
-                [0, 1, 0],
-                [-np.sin(theta), 0, np.cos(theta)]
-            ])
-
-        def Rz(theta):
-            return np.matrix([
-                [np.cos(theta), -np.sin(theta), 0],
-                [np.sin(theta), np.cos(theta), 0],
-                [0, 0, 1]
-            ])
-
-        rm = Rz(self._rotation.z) * Ry(self._rotation.y) * Rx(self._rotation.x)
-
-        # using the rotation matrix rm, get self._position and generate another
-        # point at distance 2 from self._position in the direction of the
-        # rotation.
-
-        other = np.array([
-            [2, 0, 0]
-        ]).T
-
-        other = rm * other
-
-        other = np.array([
-            other[0, 0],
-            other[1, 0],
-            other[2, 0]
-        ])
-
-        other = Vector3D(*other)
-
-        ax.quiver(
-            *self._center,
-            *other,
-            length=2,
-            color="r"
-        )
-
-        ax.view_init(0, 0)
-        ax.set_xticks([])
